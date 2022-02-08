6ebaf9303258191d696be2ab9438eec5f903b397
Turbo <91247150+TurnBoneXD@users.noreply.github.com>
2022-02-08 21:31:50 +0700

    update caesar cipher

--- Section 8/#Caesar Cipher.py
+++ Section 8/#Caesar Cipher.py
@@ -1,6 +1,3 @@
-from timeit import repeat
-
-
 print('''
 
 ░█████╗░░█████╗░███████╗░██████╗░█████╗░██████╗░  ░█████╗░██╗██████╗░██╗░░██╗███████╗██████╗░
@@ -11,32 +8,33 @@ print('''
 ░╚════╝░╚═╝░░╚═╝╚══════╝╚═════╝░╚═╝░░╚═╝╚═╝░░╚═╝  ░╚════╝░╚═╝╚═╝░░░░░╚═╝░░╚═╝╚══════╝╚═╝░░╚═╝
 
 ''')
-repeat_program = True
-while repeat_program == True:
-    alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z',
-    'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
 
+alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z',
+'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+
+def caecar(plain_text, shift_amount,cipher_direction):
+    encoded_text = ""
+    if cipher_direction == "decode":
+        shift_amount *= -1
+    for letter in plain_text:
+        if letter not in alphabet:
+            encoded_text += letter
+        else:
+            index_now = alphabet.index(letter)
+            encoded_text += alphabet[index_now+shift_amount]
+    print(f"The encoded text is {encoded_text}")
+
+
+repeat_program = True
+while repeat_program:
     direction = input("Type 'encode' to decrypt , type 'decode' to decrypt.\n").lower()
     text = input("Type your message :\n").lower()
     shift = int(input("Type the shift number :\n"))
     shift %= 26
-    
-    def caecar(plain_text, shift_amount,cipher_direction):
-        encoded_text = ""
-        if cipher_direction == "decode":
-            shift_amount *= -1
-        for letter in plain_text:
-            if letter not in alphabet:
-                encoded_text += letter
-            else:
-                index_now = alphabet.index(letter)
-                encoded_text += alphabet[index_now+shift_amount]
-        print(f"The encoded text is {encoded_text}")
-
     caecar(text,shift,direction)
-    repeat_or_not = input("Do you want to repeat ? (Y/N) :").upper()
+    repeat_or_not = input("Do you want to repeat? (Y/N) : ").upper()
     if repeat_or_not == "N":
-        repeat_or_not = False
+        repeat_program = False
 
 '''
 def encrypt(plain_text, shift_amount):
