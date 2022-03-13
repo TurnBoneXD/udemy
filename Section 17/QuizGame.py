2b0b2c930c63fe4a197db33149e73311f7a3bcf7
Turbo <91247150+TurnBoneXD@users.noreply.github.com>
2022-03-13 23:30:40 +0700

    up

--- Section 17/QuizGame.py
+++ Section 17/QuizGame.py
@@ -1,4 +1,4 @@
-from hashlib import new
+from quiz_brain import QuizBrain
 from data import question_data
 from question_model import Question
 
@@ -9,4 +9,12 @@ for question in question_data:
     new_question = Question(question_text,question_answer )
     question_bank.append(new_question)
 
-print(question_bank[0].text)
+quiz = QuizBrain(question_bank)
+
+while quiz.still_has_question():
+    quiz.next_question()
+
+print("You've completed the quiz.")
+print(f"Your final score was: {quiz.score}/{quiz.question_number}")
+
+
